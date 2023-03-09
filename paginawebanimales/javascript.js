0b6be1724f70e98260761f46979b624553e082be
rodrigo <109390532+nestorv8@users.noreply.github.com>
2023-03-08 23:14:24 -0300

    Add files via upload

---
+++ paginawebanimales/javascript.js
@@ -0,0 +1,11 @@
+
+
+//saludo inicial sweet alert //
+Swal.fire({
+    title: 'Bienvenido!',
+    imageUrl: 'https://p4.wallpaperbetter.com/wallpaper/402/740/38/funny-picture-of-dogs-underwater-chasing-wallpaper-preview.jpg',
+    
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
